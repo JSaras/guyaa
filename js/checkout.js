a8bde6ed6dcc1bdfbb3cbd09a09ac7a8133248f2
jsaras <jeff.saras@gmail.com>
2016-01-03 16:13:21 -0800

    setting up stripe. composer installed

--- js/checkout.js
+++ js/checkout.js
@@ -1,76 +1,74 @@
 $(document).ready(function() {
 
 
-$('#checkout-submit-button').on("click", function (e) {
-	e.preventDefault();
-	var quanVal = $("#quantity-input").val();
-	$("#quan-form").hide('fast');
-	$("#final-quantity-input").val(quanVal);
-	$("#cc-form").show('fast');
+        $('#checkout-submit-button').on("click", function(e) {
+            e.preventDefault();
+            var quanVal = $("#quantity-input").val();
+            $("#quan-form").hide('fast');
+            $("#final-quantity-input").val(quanVal);
+            $("#cc-form").show('fast');
 
-})
+        })
 
-$("#quantity-input").on("keyup", function (e) {
-	updatePurchaseButton();
-})
+        $("#quantity-input").on("keyup", function(e) {
+            updatePurchaseButton();
+        })
 
-$("#button-down").on("click", function (e) {
-	e.preventDefault();
-	var quanVal = $("#quantity-input").val();
-	var newQuanVal = parseInt(quanVal) - 1;
-	if(quanVal <= 0) {
-		return false;
-	} else {
-		$("#quantity-input").val("");
-		$("#quantity-input").val(newQuanVal);
-	}
+        $("#button-down").on("click", function(e) {
+            e.preventDefault();
+            var quanVal = $("#quantity-input").val();
+            var newQuanVal = parseInt(quanVal) - 1;
+            if (quanVal <= 0) {
+                return false;
+            } else {
+                $("#quantity-input").val("");
+                $("#quantity-input").val(newQuanVal);
+            }
 
-	updatePurchaseButton();
-})
+            updatePurchaseButton();
+        })
 
-$("#button-up").on("click", function (e) {
-	e.preventDefault();
-	var quanVal = $("#quantity-input").val();
-	var newQuanVal = parseInt(quanVal) + 1;
-	$("#quantity-input").val("");
-	$("#quantity-input").val(newQuanVal);
+        $("#button-up").on("click", function(e) {
+            e.preventDefault();
+            var quanVal = $("#quantity-input").val();
+            var newQuanVal = parseInt(quanVal) + 1;
+            $("#quantity-input").val("");
+            $("#quantity-input").val(newQuanVal);
 
-	updatePurchaseButton();
-})
+            updatePurchaseButton();
+        })
 
-$("#button-down-2").on("click", function (e) {
-	e.preventDefault();
-	var quanVal = $("#final-quantity-input").val();
-	var newQuanVal = parseInt(quanVal) - 1;
-	if(quanVal <= 0) {
-		return false;
-	} else {
-		$("#final-quantity-input").val("");
-		$("#final-quantity-input").val(newQuanVal);
-	}
+        $("#button-down-2").on("click", function(e) {
+            e.preventDefault();
+            var quanVal = $("#final-quantity-input").val();
+            var newQuanVal = parseInt(quanVal) - 1;
+            if (quanVal <= 0) {
+                return false;
+            } else {
+                $("#final-quantity-input").val("");
+                $("#final-quantity-input").val(newQuanVal);
+            }
 
-	updatePurchaseButton();
-})
+            updatePurchaseButton();
+        })
 
-$("#button-up-2").on("click", function (e) {
-	e.preventDefault();
-	var quanVal = $("#final-quantity-input").val();
-	var newQuanVal = parseInt(quanVal) + 1;
-	$("#final-quantity-input").val("");
-	$("#final-quantity-input").val(newQuanVal);
+        $("#button-up-2").on("click", function(e) {
+            e.preventDefault();
+            var quanVal = $("#final-quantity-input").val();
+            var newQuanVal = parseInt(quanVal) + 1;
+            $("#final-quantity-input").val("");
+            $("#final-quantity-input").val(newQuanVal);
 
-})
+        })
 
-function updatePurchaseButton () {
-	var quanVal = $("#quantity-input").val();
-	if (quanVal == 1) {
-		$("#checkout-submit-button").text("Purchase " + quanVal + " Bottle");	
-	} else {
-		$("#checkout-submit-button").text("Purchase " + quanVal + " Bottles");
-	}
-}
+        function updatePurchaseButton() {
+            var quanVal = $("#quantity-input").val();
+            if (quanVal == 1) {
+                $("#checkout-submit-button").text("Purchase " + quanVal + " Bottle");
+            } else {
+                $("#checkout-submit-button").text("Purchase " + quanVal + " Bottles");
+            }
+        }
 
-
-
-})
-// End Doc Ready
+    })
+    // End Doc Ready
